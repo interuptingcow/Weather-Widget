5cc4abce36f16b29955a60be3e4211525557d09d
NickP <npad28@pacbell.net>
2023-12-24 15:35:14 -0800

    added temp functionality

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const weatherDash = document.querySelector("#weatherDash")
 const citySearch = document.querySelector("#citySearchBtn")
 const recentSearch = document.querySelector("#recentSearch")
 const citySearchDOM = document.querySelector("#citySearchDOM")
+let displayWeather = document.querySelector("#displayWeather")
+let displayWeatherTemp = document.querySelector("#displayWeatherTemp")
+let displayWeatherTempMax = document.querySelector("#displayWeatherTempMax");
+let displayWeatherTempMin = document.querySelector("#displayWeatherTempMin")
 
 let citySearchData = [];
 
@@ -28,7 +32,7 @@ function weatherFxn(cityChosen) {
         let lon = citySearchData["0"]["lon"]
     
 
-    fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=d06852fbd917e494e3886672e9ac36cf`)
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=d06852fbd917e494e3886672e9ac36cf`)
     .then(res => {
         if(!res.ok) {
             throw new Error(`HTTP error! status: ${res.status}`);
@@ -43,6 +47,18 @@ function weatherFxn(cityChosen) {
         }
         weatherSearchData = data;
         console.log(weatherSearchData)
+        for(let i = 0; i < 1; i++){
+            let day1Div = document.createElement('li')
+            day1Div.setAttribute('id', `displayWeatherTemp${i}`);
+
+            
+            let dayWeatherTemp = weatherSearchData["list"][i]["main"]["temp"];
+            // let dayWeatherTempMax = weatherSearchData["list"][i]["main"]["temp_max"];
+            // let dayWeatherTempMin = weatherSearchData["list"][i]["main"]["temp_min"]
+            displayWeatherTemp.textContent = dayWeatherTemp;
+            // displayWeatherTempMax.textContent = dayWeatherTempMax;
+            // displayWeatherTempMin.textContent = dayWeatherTempMin;
+        }
     })
 })
 }
